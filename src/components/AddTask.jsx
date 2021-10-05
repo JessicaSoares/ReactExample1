ee73e775fd464c467cfa9ff65d57bc25c6c48c03
JessicaSoares <you@example.com>
2021-10-05 01:04:46 -0300

    Input Change

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,15 +1,28 @@
-import React, { createFactory } from 'react';
+import React, { useState } from 'react';
 
 import Button from "./Button";
 
 import "./AddTask.css";
 
 const AddTask = () => {
+    const[inputData, setInputData] = useState('');
+    const handleInputChange = (e) => {
+        
+    setInputData(e.target.value)
+    };
+
     return (
         <div className="add-task-container">
-            <input className="add-task-input" type="text"/>;
-            <Button>Adicionar</Button>
-</div>
+            <input 
+            onChange={handleInputChange} 
+            className="add-task-input" 
+            type="text"
+            />
+            ;
+            <div className="add-task-button-container">
+                <Button>Adicionar</Button>
+            </div>
+        </div>
     );
 };
  
